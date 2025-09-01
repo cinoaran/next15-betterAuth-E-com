bae984e2547df83b2d5b013c5719d19db352df8c
cinoaran <cino2304@gmail.com>
2025-09-01 06:38:29 +0200

    Build started succesfully

--- app/user/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserPage = async ({params}: {params: {id: string}}) => {
-  return <div>ID: {params.id}</div>;
-};
-
-export default UserPage;
--- helpers/getServerSession.ts
+++ helpers/getServerSession.ts
@@ -3,11 +3,11 @@
 import {auth} from "@/lib/auth"; // path to your Better Auth server instance
 import {headers} from "next/headers";
 
-const session = await auth.api.getSession({
-  headers: await headers(), // you need to pass the headers object.
-});
-
 export async function getServerSession() {
+  const session = await auth.api.getSession({
+    headers: await headers(), // you need to pass the headers object.
+  });
+
   try {
     return session ?? null;
   } catch (error) {
